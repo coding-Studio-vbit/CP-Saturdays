60962cea9d865ad1b113626dbac138c8a28d3839
chandrakiran343 <chandra.dkiran@outlook.com>
2022-01-22 20:18:16 +0530

    lEEt code easy probs :)

---
+++ ChandraKiran/22-1-22/twoSum.js
@@ -0,0 +1,10 @@
+var twoSum = function(nums, target) {
+    for(let i=0;i < nums.length;i++){
+        for(let j = i+1;j<nums.length;j++){
+            if(nums[j] === target - nums[i]){
+                return([i,j])
+            }
+        }
+    }
+    
+};
